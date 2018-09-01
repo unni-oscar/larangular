7076eb6886babec2f3464cfb112636902d43ae37
Unni <unni@soupedup.com>
2018-09-01 14:03:07 +0530

    Implemented JWT

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,18 +1,11 @@
-<?php
+Route::group([
+'middleware' => 'api',
+'prefix' => 'auth'
 
-use Illuminate\Http\Request;
+], function ($router) {
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login');
+Route::post('logout', 'AuthController@logout');
+Route::post('refresh', 'AuthController@refresh');
+Route::post('me', 'AuthController@me');
 });
