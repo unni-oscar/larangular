dde951ff07ae15d2c8fa91f6ad2c60f94048ca9a
Unni <unni@soupedup.com>
2018-09-03 11:49:06 +0530

    login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php 
 
 Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'middleware' => 'api'
 ], function ($router) {
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
